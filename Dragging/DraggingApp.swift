337ba65bfd46eab6ad2bc559593ccae606a9cea7
David Whetstone <david@humblehacker.com>
2024-02-09 14:15:32 -0800

    Initial Commit

---
+++ Dragging/DraggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggingApp.swift
+//  Dragging
+//
+//  Created by David Whetstone on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DraggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
